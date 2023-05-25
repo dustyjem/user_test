3b56d46171eafe91a14a2330c7a9feb96d4b9ac1
dustyjem <97073289+dustyjem@users.noreply.github.com>
2023-05-25 01:17:56 -0600

    first commit

---
+++ routes/contacts.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require('express').Router();
+
+const myControllers = require('../controllers/index.js');
+
+
+console.log('Something is happening here');
+routes.get('/', myControllers.getAll);
+routes.get('/:id', myControllers.getSingle);
+routes.post('/', myControllers.create);
+routes.put('/:id', myControllers.update);
+routes.delete('/:id', myControllers.remove);
+
+module.exports = routes;
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const contactsRouter = require('./routes/contacts');
+
+app.use(express.json()); // Parse JSON-encoded bodies
+app.use('/contacts', contactsRouter);
+
+// Serve the HTML file containing the form
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'resume_input.html'));
+});
+
+// Start the server
+const mongodb = require('./db/db');
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.error('Error initializing database:', err);
+    process.exit(1);
+  }
+
+  app.listen(3535, function () {
+    console.log('Server is running on http://localhost:3535');
+  });
+});
